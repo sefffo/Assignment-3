13586186e2958025dd2bf07cc1bc2e910352910a
sefffo <154231473+sefffo@users.noreply.github.com>
2025-06-29 15:03:01 +0300

    Add files via upload
    added by upload

---
+++ assignemt3/assignemt3/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace assignemt3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region 1
+            //int num;
+            //num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num);
+
+            #endregion
+
+
+            #region 2
+
+            //string s="saif";
+            //Convert.ToInt32(s);
+            // it will give an error 
+
+            #endregion
+
+
+            #region 3
+
+            //float num1, num2;
+
+            //num1 = 2.5523f;
+            //num2 = 3.453f;
+
+            //Console.WriteLine(num1 + num2);
+            // it will approximate the values
+
+
+
+            #endregion
+            #region 4
+
+            //string s = "saif lotfy";
+
+            //Console.WriteLine(s.Substring(0, 3));
+
+            #endregion
+            #region 5
+
+
+            //string s="saif";
+            //string l = "lotfy";
+
+            //string m = s+ l;
+
+            //Console.WriteLine(m);
+
+
+            #endregion
+            #region 6
+            //double p, r, t;
+            //p=double.Parse(Console.ReadLine());
+            //r=double.Parse(Console.ReadLine());
+            //t=double.Parse(Console.ReadLine());
+
+            //double F = (p * r * t) / 100;
+            //Console.WriteLine(F);
+
+            #endregion
+            #region 7
+
+            //double bmi;
+            //double h,w;
+
+            //w=double.Parse(Console.ReadLine());
+            //h = double.Parse(Console.ReadLine());
+            //bmi = w / (h * h);
+            //Console.WriteLine(bmi);
+            #endregion
+
+
+            #region 8
+            //Console.Write("Enter day: ");
+            //int day = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter month: ");
+            //int month = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter year: ");
+            //int year = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine($"\nToday's date : {day} , {month} , {year}");
+            //Console.WriteLine($"Today's date : {day} / {month} / {year}");
+            //Console.WriteLine($"Today's date : {day} – {month} – {year}");
+
+            #endregion
+
+
+            #region 9
+
+            //The code creates a specific date and formats it as MM / dd / yyyy.
+
+            //The output will be:
+
+            //The event is on 06/14/2024
+
+
+            #endregion
+
+            #region 10
+
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //( inverse of the inner braces is 1 (0))
+
+            //so it will carry the value 1
+
+
+
+            #endregion
+
+            #region 11
+
+
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+
+            //6 1 because each opreation will be done and these + signs only for cocncatenation 
+
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ assignment2.cs
@@ -0,0 +1,66 @@
+﻿namespace Assignemt2
+{
+
+    class myclass
+    {
+        public int num;
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1
+            //    int s;
+            //    s=int.Parse(Console.ReadLine());
+            //    Console.WriteLine(s);
+            //}
+            #endregion
+
+            #region 2
+
+            //int a = 10;
+            //int b = 5;
+
+
+            //b = a;
+            //b = 20;
+
+
+            //b will change and a still with no change
+
+
+
+
+            #endregion
+
+            #region 3
+
+               myclass obj1 = new myclass();
+               myclass obj2 = new myclass();
+
+               obj1.num = 10;
+               obj2.num = 20;
+
+               obj2 = obj1;
+
+               obj2.num = 30;
+                //obj
+
+               Console.WriteLine(obj1.num);
+               Console.WriteLine(obj2.num);
+                                           
+               
+
+                
+
+
+            #endregion
+
+
+
+
+        }
+    }
+}
